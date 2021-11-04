de6d127bd7eea3802eafc3503921f0ca9472e3b9
Jeremias Jutz <jutz.jeremias@gmail.com>
2021-11-04 21:07:41 +0100

    Add Image cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/tailwind.css';
 
 import Link from 'next/link';
 import SimpleReactLightbox from 'simple-react-lightbox';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AnimatePresence, motion, Inertia, Tween } from 'framer-motion';
 import type { AppProps } from 'next/app';
 
@@ -10,7 +10,7 @@ import projects, { Project } from '../projects';
 import { nameToSlug } from '../utils';
 import { ThemeProvider, useTheme } from 'next-themes';
 import DarkModeSwitcher from '../components/DarkModeSwitcher';
-import { RiHomeLine } from 'react-icons/ri';
+import { RiHomeLine, RiLoader2Line, RiLoaderLine } from 'react-icons/ri';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const springConfig: Tween = {
@@ -41,59 +41,99 @@ function MyApp({ Component, pageProps }: AppProps) {
   };
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const [isLoading, setIsLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {
+    cacheImages(projects.flatMap(({ images }) => images.map(({ url }) => url)));
+  }, []);
+
+  const cacheImages = async (arr: string[]) => {
+    const promises = arr.map((src: string, i) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = (e) => {
+          setProgress((i + 1) / arr.length);
+          resolve(e);
+        };
+        img.onerror = reject;
+      });
+    });
+    await Promise.all(promises);
+    setIsLoading(false);
+  };
+
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
       <SimpleReactLightbox>
-        <AnimatePresence exitBeforeEnter>
-          <Link href="/" passHref>
-            <a className="fixed z-30 grid w-12 h-12 text-white border-b border-white cursor-pointer mix-blend-difference place-items-center top-6 left-6">
-              <RiHomeLine />
-            </a>
-          </Link>
-          <div className="fixed inset-y-0 left-0 z-20 grid items-center w-12 my-6 ml-6 border-r border-white group mix-blend-difference">
-            <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="flex justify-center text-lg text-white transition-all duration-700 -rotate-90 rounded w-11 group-hover:font-semibold"
-            >
-              {isMenuOpen ? 'schliessen' : 'menu'}
-            </button>
+        {isLoading ? (
+          <div className="fixed inset-0 grid place-items-center">
+            <div className="w-full text-center">
+              <p className="text-xl font-medium">
+                Inhalte laden{' '}
+                <span className="tabular-nums">
+                  {Math.round(progress * 100)}%
+                </span>
+              </p>
+              <div className="w-1/2 mx-auto mt-4">
+                <div
+                  className="w-full h-[2px] transition-transform origin-left bg-black dark:bg-white"
+                  style={{ transform: `scaleX(${progress})` }}
+                ></div>
+              </div>
+            </div>
           </div>
-          <DarkModeSwitcher />
-          <AnimatePresence>
-            {isMenuOpen ? (
-              <motion.div
-                variants={container}
-                initial="hidden"
-                animate="show"
-                exit="hidden"
-                className="fixed inset-0 z-10 grid pl-12 m-6 overflow-y-auto text-black bg-white ring-1 ring-black dark:text-white dark:bg-black md:grid-cols-3 dark:ring-white"
+        ) : (
+          <AnimatePresence exitBeforeEnter>
+            <Link href="/" passHref>
+              <a className="fixed z-30 grid w-12 h-12 text-white border-b border-white cursor-pointer mix-blend-difference place-items-center top-6 left-6">
+                <RiHomeLine />
+              </a>
+            </Link>
+            <div className="fixed inset-y-0 left-0 z-20 grid items-center w-12 my-6 ml-6 border-r border-white group mix-blend-difference">
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="flex justify-center text-lg text-white transition-all duration-700 -rotate-90 rounded w-11 group-hover:font-semibold"
               >
-                {projects.map(({ name }: Project, i) => {
-                  return (
-                    <div
-                      key={name}
-                      className={`border-black dark:border-white flex items-center ${
-                        i === projects.length - 1 ? '' : 'border-b'
-                      } ${i % 3 === 2 ? '' : 'md:border-r'}`}
-                    >
-                      <Link href={nameToSlug(name)} passHref>
-                        <motion.a
-                          variants={item}
-                          onClick={() => setIsMenuOpen(false)}
-                          className={`px-12 py-6 text-3xl transition-colors duration-700 origin-left  hover:text-indigo-500`}
-                        >
-                          {name}
-                        </motion.a>
-                      </Link>
-                    </div>
-                  );
-                })}
-              </motion.div>
-            ) : null}
+                {isMenuOpen ? 'schliessen' : 'menu'}
+              </button>
+            </div>
+            <DarkModeSwitcher />
+            <AnimatePresence>
+              {isMenuOpen ? (
+                <motion.div
+                  variants={container}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  className="fixed inset-0 z-10 grid pl-12 m-6 overflow-y-auto text-black bg-white ring-1 ring-black dark:text-white dark:bg-black md:grid-cols-3 dark:ring-white"
+                >
+                  {projects.map(({ name }: Project, i) => {
+                    return (
+                      <div
+                        key={name}
+                        className={`border-black dark:border-white flex items-center ${
+                          i === projects.length - 1 ? '' : 'border-b'
+                        } ${i % 3 === 2 ? '' : 'md:border-r'}`}
+                      >
+                        <Link href={nameToSlug(name)} passHref>
+                          <motion.a
+                            variants={item}
+                            onClick={() => setIsMenuOpen(false)}
+                            className={`px-12 py-6 text-3xl transition-colors duration-700 origin-left  hover:text-indigo-500`}
+                          >
+                            {name}
+                          </motion.a>
+                        </Link>
+                      </div>
+                    );
+                  })}
+                </motion.div>
+              ) : null}
+            </AnimatePresence>
+            <Component {...pageProps} />
           </AnimatePresence>
-          <Component {...pageProps} />
-        </AnimatePresence>
+        )}
       </SimpleReactLightbox>
     </ThemeProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -83,7 +83,6 @@ const Home = () => {
         {src !== '' ? (
           <motion.img
             ref={imageRef}
-            key="child"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
